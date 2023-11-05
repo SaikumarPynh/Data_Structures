82843271bef8887c48eb7d16790a1316adbd5a0d
SaikumarPynh <saikumarpynh@gmail.com>
2023-11-05 19:30:14 +0530

    Binary search using recursion

---
+++ BinarySearch.py
@@ -0,0 +1,21 @@
+def binarysearch(arr,i,j,x):
+    if j >= i:
+        mid = (i + j) // 2
+        if arr[mid] == x:
+            return 'element found at pos', mid
+            
+        elif arr[mid] > x:
+            return binarysearch(arr,i,mid-1,x)
+        else:
+            return binarysearch(arr,mid+1,j,x)
+    else:
+        return 'not found'
+        
+
+    
+
+
+arr = [2,5,2,2,6,8,9,3]
+key = int(input("enter the element to be search:"))
+result = binarysearch(arr,0,len(arr),key)
+print(result)
