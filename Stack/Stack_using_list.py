d878685f4f6164cd995e17e0d5134981f06c7184
SaikumarPynh <saikumarpynh@gmail.com>
2023-11-08 08:49:22 +0530

    Display method adding

--- Stack/Stack_using_list.py
+++ Stack/Stack_using_list.py
@@ -22,24 +22,27 @@ class Stack:
     
     def is_empty(self):
         return len(self.list) == 0
-
+    def Display(self):
+        for i in self.list:
+            print(i)
 obj = Stack(5)
 while True:
-    print("1.push\n2.pop\n3.peek\n4.is_empty")
+    print("1.push\n2.pop\n3.peek\n4.is_empty\n5.Display")
     choice = int(input("enter the choice:"))
     if choice == 1:
         item = int(input("enter the element to be push:"))
         obj.push(item)
     elif choice == 2:
         obj.pop()
-        
-
     elif choice == 3:
         peek_ele = obj.peek()
         print("peek element is :",peek_ele)
     elif choice == 4:
         result = obj.is_empty()
         print(result)
+    elif choice == 5:
+        obj.Display()
+    
     else:
         print("invalid choice:")
         break
